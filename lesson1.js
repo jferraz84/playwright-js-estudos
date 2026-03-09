f590d8486d02d04563cce454ca5fd4cc17c2cb2d
Jorge Ferraz <jferrazjorge@gmail.com>
2026-03-09 18:20:13 -0300

    Tipos de Variaveis e Constantes em JavaScript

--- lesson1.js
+++ lesson1.js
@@ -1,2 +1,38 @@
 // Hello World in JavaScript
 console.log("Hello, World!");
+
+//Variables
+var firstName = "Jorge";
+let lastName = "Ferraz";
+var city = "São Paulo - SP";
+
+var age, dateOfBirth, sex;
+age = 41;
+dateOfBirth = "1984-12-29";
+sex = "Masculino";
+
+console.log(firstName, lastName, age + " anos,", sex, "nascido em: " + dateOfBirth, city);
+
+age = 42;
+console.log("Agora tenho " + age + " anos.");
+
+//constantes
+const country = "Brasil";
+const ocupation = "Desenvolvedor de Software";
+console.log("Eu moro no " + country + ".");
+console.log("Eu trabalho como " + ocupation + "."); 
+console.log("------------------------------");
+
+//data types
+var middleName = "Henrique"; //string
+var ageOfBorther = 25; //number
+var isStudent = false; //boolean
+var isMarried = true;//boolean
+var yearsInMarriage = null; //null
+var numberOfCars = undefined; //object
+console.log("Meu nome do meio é " + middleName + ".");
+console.log("Meu irmão tem " + ageOfBorther + " anos.");
+console.log("Sou estudante? " + isStudent);
+console.log("Sou casado? " + isMarried);
+console.log("Anos de casamento: " + yearsInMarriage);
+console.log("Número de carros: " + numberOfCars);       
